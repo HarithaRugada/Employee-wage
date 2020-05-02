1c00b97c465882d4c4f5d5244f41494aac87d7ee
HarithaRugada <haritharugada@gmail.com>
2020-05-02 12:50:24 +0530

    master

--- empwage.sh
+++ empwage.sh
@@ -1,10 +1,3 @@
 #! /bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
-present=1
-status=$(( $RANDOM%2 ))
-if [ $status -eq $present ]
-then
-	echo "present"
-else
-	echo "absent"
-fi
+
