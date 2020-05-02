9e8f2a580ad36e8d55327e0ed5ce547d0284ee20
HarithaRugada <haritharugada@gmail.com>
2020-05-02 13:18:13 +0530

    added usecase5

--- empwage.sh
+++ empwage.sh
@@ -1,21 +1,31 @@
-#! /bin/bash -x
+#! /bin/bash/ -x
+
 echo "Welcome to Employee Wage Computation Program"
 present=1
-status=$(( $RANDOM%2 ))
 hourwage=20
 salary=0
-if [ $status -eq $present ]
-then
-	workhours=$(( $RANDOM%10 ))
+totalwh=0
+for(( days=1;days<=20;days++ ))
+do
+        status=$(( $RANDOM%2 ))
+        if [ $status -eq $present ]
+        then
+                echo "employee is present"
+                workhours=$(( $RANDOM%10 ))
                 #adding 1 to workhours to avoid the case when workhour is zero and being marked as present
                 case $workhours in
                         9) echo "Full time"
-                                salary=$(( $(( $workhours+1 ))*$hourwage ))
+                                salary=$(( $salary+$(( $(( $workhours+1 ))*hourwage )) ))
+                                totalwh=$(( $totalwh+$(( $workhours+1 )) ))
                                 ;;
                         *) echo "Part time"
-                                salary=$(( $(( $workhours+1 ))*$hourwage ))
+                                salary=$(( $salary+$(( $(( $workhours+1 ))*hourwage )) ))
+                                totalwh=$(( $totalwh+$(( $workhours+1)) ))
                                 ;;
                 esac
-else
-	echo "absent"
-fi
+        else
+                echo "employee is absent"
+        fi
+done
+echo "Month salary:" $salary
+echo "Total worked hours: "$totalwh
