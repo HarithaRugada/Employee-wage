2a327f28abf5fc748610ef33a0c852d5e1604d27
HarithaRugada <haritharugada@gmail.com>
2020-05-02 12:49:28 +0530

    added usecase1

--- empwage.sh
+++ empwage.sh
@@ -1,2 +1,10 @@
 #! /bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
+present=1
+status=$(( $RANDOM%2 ))
+if [ $status -eq $present ]
+then
+	echo "present"
+else
+	echo "absent"
+fi
